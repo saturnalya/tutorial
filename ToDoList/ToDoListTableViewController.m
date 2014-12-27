8587810d9b6925bef20098da1f5b6926b8043e3c
saturnalya jones <youwishyouknewmyemail@yourbutt.com>
2014-12-27 10:33:18 -0800

    user can add item to list

--- ToDoList/ToDoListTableViewController.m
+++ ToDoList/ToDoListTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ToDoListTableViewController.h"
 #import "ToDoItem.h"
+#import "AddToDoItemViewController.h"
 
 @interface ToDoListTableViewController ()
 
@@ -16,16 +17,21 @@
 @end
 
 @implementation ToDoListTableViewController
-- (IBAction)unwindToList:(UIStoryboardSegue *)segue {
-}
 
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue {
+    AddToDoItemViewController *source = [segue sourceViewController];
+    ToDoItem *item = source.toDoItem;
+    if (item != nil) {
+        [self.toDoItems addObject:item];
+        [self.tableView reloadData];
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.toDoItems = [[NSMutableArray alloc] init];
     [self loadInitialData];
 }
-    
 
 - (void)loadInitialData {
     ToDoItem *item1 = [[ToDoItem alloc] init];
